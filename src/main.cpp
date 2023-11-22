beb7e462a9007557368605b302b7813713229ad2
tonto2423 <tnt.kosen2423@gmail.com>
2023-11-22 11:53:52 +0900

    no ok

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,12 @@ int main() {
         cout << "name:" << info.port() << "\n" << endl;
     }
 
+    Serial::Config config;
+    config.baudRate = 115200;
+    config.byteSize = 8;
+    config.parity = Serial::Config::Parity::NO;
+    config.stopBits = Serial::Config::StopBits::ONE;
+
     Serial serial;
     int port;
     cin >> port;
@@ -20,10 +26,16 @@ int main() {
     if (!serial.open(list[port], 115200))
         return -1;
     //SerialInfo構造体にポート名とデバイス名が入っている
+    serial.setConfig(config);
     SerialInfo info = serial.getInfo();
     cout << "open success" << endl;
     cout << "device name:" << info.device_name() << endl;
     cout << "name:" << info.port() << "\n" << endl;
+    Serial::Config config_ = serial.getConfig();
+    cout << "baudRate:" << config_.baudRate << endl;
+    cout << "byteSize:" << config_.byteSize << endl;
+    cout << "parity:" << (int)config_.parity << endl;
+    cout << "stopBits:" << (int)config_.stopBits << endl;
     //以下ループ
     while (true) {
         auto v = serial.read();
